0ab49de43ce1717848d06708a7307b34016ebc48
Victor <128171129+V1ct0rR0ch4976@users.noreply.github.com>
2023-08-01 09:07:15 -0300

    Atualizando páginas

--- src/components/pages/Contact/contact.tsx
+++ src/components/pages/Contact/contact.tsx
@@ -1,3 +1,19 @@
+import Button from "../../layout/ButtonPurple/button";
+import Container from "../../layout/Container/container";
+import CompanyDescription from "../Company/CompanyDesciption/companyDesciption";
+import  './contact.scss'
+
 export default function Contact() {
-    return <><h1>Contato</h1></>
+    return (
+        <Container customClass="start container">
+            <CompanyDescription className="descriptionContainer">
+                <h1>
+                    Converse conosco!
+                </h1>
+                <p>Use nosso chat interativo para falar com nossos funcionários, para te ajudarem pro que der e vier!</p>
+                <Button text="Vamos conversar!" type="button" id="btnLetsTalk"/>
+            </CompanyDescription>
+            <img src="https://www.pmvc.ba.gov.br/wp-content/uploads/telefone-pessoa-ligando.jpg" alt="FOto de uma pessoa atendendo um telefone" className="chatImage"/>
+        </Container>
+    )
 }
